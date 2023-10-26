8b79c6dc59d3b5a0e1ef9a7dc08d7fc2507e5d9f
Smysla <148914532+Smysla@users.noreply.github.com>
2023-10-26 14:50:00 +0200

    Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,33 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+	Wektor2D(double wspX = 0, double wspY = 0)
+	{
+		x = wspX;
+		y = wspY;
+	}
+
+	~Wektor2D()
+	{
+	}
+	void setX(double wspx) { x = wspx; }
+	double getX() { return x; }
+	void setY(double wspy) { y = wspy; }
+	double getY() { return y; }
+
+private:
+	double x;
+	double y;
 };
+
+Wektor2D operator+(Wektor2D& wek1, Wektor2D& wek2)
+{
+	double x1 = wek1.getX(), x2 = wek2.getX(), y1=wek1.getY(), y2=wek2.getY();
+	return Wektor2D(x1+x2, y1+y2);
+}
+
+double operator*(Wektor2D& wek1, Wektor2D& wek2)
+{
+	double x1 = wek1.getX(), x2 = wek2.getX(), y1 = wek1.getY(), y2 = wek2.getY();
+	return x1 * x2 + y1 * y2;
+}
